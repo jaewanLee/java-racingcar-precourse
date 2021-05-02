577956bc0bb7a109165ad6662dcfbc4035dbfaf6
jaewanLee <ianlee9180@gmail.com>
2021-05-02 18:27:17 +0900

    feat : RacingCars:validation 추가
    정상적인 경주가 가능하도록 리스트 생성시 자동차가 두대 이상인지 확인한다.

--- src/main/java/RacingCars.java
+++ src/main/java/RacingCars.java
@@ -5,6 +5,9 @@ public class RacingCars {
     private ArrayList<Car> cars=new ArrayList<>();
 
     RacingCars(String[] names) {
+        if (names.length < 2) {
+            throw new IllegalArgumentException("경주할 자동차 2대 이상의 이름을 ','로 구분하여 입력해주세요.");
+        }
         for (String name : names) {
             cars.add(new Car(name));
         }
