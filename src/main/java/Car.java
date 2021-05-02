b3670e31ea0ce76aeffd50b7c564a6a9e4189808
jaewanLee <ianlee9180@gmail.com>
2021-05-02 18:08:23 +0900

    feat : CarClass 생성자 Validation 추가
    - Car(String name) - 주어진 name의 길이가 5이하인지 검증

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,7 +2,12 @@ public class Car {
     private String name;
     private int moving;
 
+    private Number maxNameLength = new Number(5);
+
     public Car(String name) {
+        if (name.length() > this.maxNameLength.getValue()) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
+        }
         this.name = name;
         this.moving = 0;
     }
