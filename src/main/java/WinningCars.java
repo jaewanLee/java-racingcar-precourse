6b4419326fcaf107015627c6204eb4f1845d56a0
jaewanLee <ianlee9180@gmail.com>
2021-05-02 18:32:25 +0900

    feat : WinningCars:winningMessage()
    우승 자동차 리스트에서 자동차 명과 문구를 출력할 수 있는 func을 추가한다

--- src/main/java/WinningCars.java
+++ src/main/java/WinningCars.java
@@ -12,5 +12,11 @@ public class WinningCars {
         this.winningCars.clear();
     }
 
-
+    public String winningMessage() {
+        String[] carNames = new String[this.winningCars.size()];
+        for (int i = 0; i < this.winningCars.size(); i++) {
+            carNames[i] = this.winningCars.get(i).getName();
+        }
+        return String.join(",", carNames) + "가 최종 우승했습니다";
+    }
 }
